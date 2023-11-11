ef59539847f97e5212113eb05dd729b1e7797ff2
vishaljain840 <57666897+vishaljain840@users.noreply.github.com>
2023-11-11 08:42:13 -0500

    changes for login page

--- internetProject/views.py
+++ internetProject/views.py
@@ -1,12 +1,33 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
 # Create your views here.hghghg
 
 def home(request):
-    return render(request, "internetProject/signup.html")
+    return render(request, "internetProject/index.html")
 
 def signup(request):
+
+    if request.method == "POST":
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+
+        myuser.save()
+
+        messages.success(request, "Your account has been successfully created.")
+
+        return redirect('signin')
+
+
     return render(request, "internetProject/signup.html")
 
 def signin(request):
