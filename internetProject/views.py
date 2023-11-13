efcf98df4e2c952b6b2fa5c4834d38c335e2bb68
vishaljain840 <57666897+vishaljain840@users.noreply.github.com>
2023-11-13 18:38:18 -0500

    email sending functionality now works

--- internetProject/views.py
+++ internetProject/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth import authenticate, login, logout
+from django.core.mail import send_mail
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import messages
+from internet_project import settings
 # Create your views here.hghghg
 
 def home(request):
@@ -18,6 +20,24 @@ def signup(request):
         pass1 = request.POST['pass1']
         pass2 = request.POST['pass2']
 
+        if User.objects.filter(username=username):
+            messages.error(request, "Username already exists! Please try some other username")
+            return redirect('home')
+
+        if User.objects.filter(email=email):
+            messages.error(request, "Email already registered")
+            return redirect('home')
+
+        if len(username)>10:
+            messages.error(request, "Passwords didn't match")
+
+        if pass1 != pass2:
+            messages.error(request, "Passwords didn't match!")
+
+        if not username.isalnum():
+            messages.error(request, "Username must be Alpha-Numeric!")
+            return redirect('home')
+
         myuser = User.objects.create_user(username, email, pass1)
         myuser.first_name = fname
         myuser.last_name = lname
@@ -25,6 +45,14 @@ def signup(request):
         myuser.save()
 
         messages.success(request, "Your account has been successfully created.")
+
+        # Welcome Email
+
+        subject = "Welcome to my App"
+        message = "Hello" + myuser.first_name + "!! \n" + "Welcome to my App!! \n Thank you for visiting our website \n We have also sent you a confirmation email, please confirm your email address to activate your account. \n\n Thanking you\n "
+        from_email = settings.EMAIL_HOST_USER
+        to_list = [myuser.email]
+        send_mail(subject, message, from_email, to_list)
 
         return redirect('signin')
 
--- internet_project/info.py
+++ internet_project/info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'appcurrency06@gmail.com'
-EMAIL_HOST_PASSWORD = 'xspxamhgoqyvadbf'
+EMAIL_HOST_PASSWORD = 'ssmcoijldmepcivy'
 EMAIL_PORT = 587
