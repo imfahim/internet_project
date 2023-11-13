033eb5b7c6b726c90fed4eaf39f4b701753c7710
vishaljain840 <57666897+vishaljain840@users.noreply.github.com>
2023-11-13 10:01:16 -0500

    login, signup, confirmation mail

--- internetProject/views.py
+++ internetProject/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
@@ -31,9 +32,25 @@ def signup(request):
     return render(request, "internetProject/signup.html")
 
 def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        pass1 = request.POST['pass1']
+
+        user = authenticate(username=username, password=pass1)
+
+        if user is not None:
+            login(request, user)
+            fname = user.first_name
+            return render(request, "internetProject/index.html",{'fname': fname})
+        else:
+            messages.error(request, "Bad Credentials")
+            return redirect('home')
     return render(request, "internetProject/signin.html")
 
 def signout(request):
-   pass
+   logout(request)
+   messages.success(request, "Logged Out Successfully!")
+   return redirect('home')
+
 
 
