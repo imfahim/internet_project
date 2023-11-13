a01fd5069db3399a217e5c0aa95a03124526e9a1
Shreyansh Dalwadi <shreyanshdalwadi@gmail.com>
2023-11-12 21:31:14 -0500

    Base Html

--- internet_project/urls.py
+++ internet_project/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for internet_project project.
+URL configuration for DistanceEd project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='internetProject/')),
     path('admin/', admin.site.urls),
-    path(r'currency_exchange/', include('currency_exchange.urls')),
+    path('internetProject/', include('internetProject.urls')),
 ]
