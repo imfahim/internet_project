703c1fda5d3005e153c590be60b52f4fe5b725a1
yuanfanfan <yuan981130@163.com>
2023-11-14 00:45:32 -0500

    can get one rate from the api

--- currency_exchange/urls.py
+++ currency_exchange/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 from currency_exchange import views
 
 urlpatterns = [
-    path(r'currency/<str:from_currency>/<str:to_currency>/', views.currency, name='currency'),
+    path(r'currency/<str:from_currency>/', views.currency, name='currency'),
+
 ]
