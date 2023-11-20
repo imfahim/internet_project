06f63777276f986886df639c670ca039f05303c1
Raja Babu <121944643+rajab765@users.noreply.github.com>
2023-11-20 09:46:10 -0500

    Landing Page Table's Columns Properties Features wrt +ve/-ve Change
    Completely Working/Visible in UI

--- internetProject/views.py
+++ internetProject/views.py
@@ -51,31 +51,29 @@ def print_crypto_info(data):
 # Create your views here.
 def index(request):
     fxTableData = [
-        {'id': 1, 'Name': 'Bitcoin', 'Price': 50000, 'oneHrPer': 50000, 'twoHrPer': 50000, 'sevenDayPer': 50000,
-         'Market_Cap': 1000000000000, 'Volume_24h': 5, 'Circulating_Supply': 5},
-        {'id': 2, 'Name': 'Bitcoin', 'Price': 50000, 'oneHrPer': 50000, 'twoHrPer': 50000, 'sevenDayPer': 50000,
-         'Market_Cap': 1000000000000, 'Volume_24h': 5, 'Circulating_Supply': 5},
-        {'id': 3, 'Name': 'Bitcoin', 'Price': 50000, 'oneHrPer': 50000, 'twoHrPer': 50000,
+        {'id': 1, 'Name': 'Bitcoin', 'Price': 50000,'oneHrPer': 50000,'twoHrPer': -50000,'sevenDayPer': 50000, 'Market_Cap': 1000000000000, 'Volume_24h': 5,'Circulating_Supply': 5},
+        {'id': 2, 'Name': 'Bitcoin', 'Price': 50000,'oneHrPer': 50000,'twoHrPer': 50000,'sevenDayPer': 50000, 'Market_Cap': 1000000000000, 'Volume_24h': 5,'Circulating_Supply': 5},
+        {'id': 3, 'Name': 'Bitcoin',  'Price': 50000, 'oneHrPer': -50000, 'twoHrPer': 50000,
          'sevenDayPer': 50000, 'Market_Cap': 1000000000000, 'Volume_24h': 5, 'Circulating_Supply': 5},
-        {'id': 4, 'Name': 'Bitcoin', 'Price': 50000, 'oneHrPer': 50000, 'twoHrPer': 50000,
+        {'id': 4, 'Name': 'Bitcoin',  'Price': 50000, 'oneHrPer': 50000, 'twoHrPer': 50000,
          'sevenDayPer': 50000, 'Market_Cap': 1000000000000, 'Volume_24h': 5, 'Circulating_Supply': 5},
-        {'id': 4, 'Name': 'Bitcoin', 'Price': 50000, 'oneHrPer': 50000, 'twoHrPer': 50000,
+        {'id': 4, 'Name': 'Bitcoin',  'Price': 50000, 'oneHrPer': 50000, 'twoHrPer': -50000,
          'sevenDayPer': 50000, 'Market_Cap': 1000000000000, 'Volume_24h': 5, 'Circulating_Supply': 5},
-        {'id': 5, 'Name': 'Bitcoin', 'Price': 50000, 'oneHrPer': 50000, 'twoHrPer': 50000,
+        {'id': 5, 'Name': 'Bitcoin',  'Price': 50000, 'oneHrPer': 50000, 'twoHrPer': 50000,
          'sevenDayPer': 50000, 'Market_Cap': 1000000000000, 'Volume_24h': 5, 'Circulating_Supply': 5},
-        {'id': 6, 'Name': 'Bitcoin', 'Price': 50000, 'oneHrPer': 50000, 'twoHrPer': 50000,
+        {'id': 6, 'Name': 'Bitcoin',  'Price': 50000, 'oneHrPer': 50000, 'twoHrPer': 50000,
          'sevenDayPer': 50000, 'Market_Cap': 1000000000000, 'Volume_24h': 5, 'Circulating_Supply': 5},
-        {'id': 7, 'Name': 'Bitcoin', 'Price': 50000, 'oneHrPer': 50000, 'twoHrPer': 50000,
-         'sevenDayPer': 50000, 'Market_Cap': 1000000000000, 'Volume_24h': 5, 'Circulating_Supply': 5},
+        {'id': 7, 'Name': 'Bitcoin', 'Price': 50000, 'oneHrPer': -50000, 'twoHrPer':50000,
+         'sevenDayPer': -50000, 'Market_Cap': 1000000000000, 'Volume_24h': 5, 'Circulating_Supply': 5},
 
     ]
     for rowData in fxTableData:
         # Add a 'change_color' attribute to each entry based on the value of 'change_24h'
-        rowData['changeStatus'] = 'positiveChange' if rowData['oneHrPer'] > 0 else 'negativeChange'
-        rowData['changeStatus'] = 'positiveChange' if rowData['twoHrPer'] > 0 else 'negativeChange'
-        rowData['changeStatus'] = 'positiveChange' if rowData['sevenDayPer'] > 0 else 'negativeChange'
+        rowData['change1Status'] = 'positiveChange' if rowData['oneHrPer'] > 0 else 'negativeChange'
+        rowData['change2Status'] = 'positiveChange' if rowData['twoHrPer'] > 0 else 'negativeChange'
+        rowData['change7Status'] = 'positiveChange' if rowData['sevenDayPer'] > 0 else 'negativeChange'
 
-    return render(request, 'internetProject/index.html', {'fxTableData': fxTableData})
+    return render(request, 'internetProject/index.html',{'fxTableData':fxTableData})
 
 
 def home(request):
