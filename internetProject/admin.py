bf036cf3eaed1eecd148e3be875b385db426b480
jaykshan <59968950+jk1807@users.noreply.github.com>
2023-11-25 16:31:10 -0500

    payment model
    Updated the payment model

--- internetProject/admin.py
+++ internetProject/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from internetProject.models import Complaint, Feedback
+from internetProject.models import Complaint, Feedback, Payment
 
 # Register your models here.
 admin.site.register(Complaint)
 admin.site.register(Feedback)
+
+admin.site.register(Payment)
--- internetProject/models.py
+++ internetProject/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class Currency_rate(models.Model):
@@ -32,3 +34,32 @@ class Feedback(models.Model):
 
     def __str__(self):
         return self.title
+
+
+# For the payment
+# class Payment(models.Model):
+#     first_name = models.CharField(max_length=50)
+#     last_name = models.CharField(max_length=50)
+#     card_number = models.CharField(max_length=16)
+#     card_expiry_month = models.CharField(max_length=2)
+#     card_expiry_year = models.CharField(max_length=4)
+#     card_cvc = models.CharField(max_length=3)
+#     # Add other payment-related fields as needed
+#
+#     def __str__(self):
+#         return f'{self.first_name} {self.last_name} - {self.card_number} - {self.card_expiry_month}/{self.card_expiry_year}'
+
+
+
+
+
+class Payment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    card_number = models.CharField(max_length=16)
+    id_number = models.CharField(max_length=20)
+    transaction_id = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.transaction_id}"
